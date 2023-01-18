df67ab69137aca5801f55e6d3c327e560681f93d
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-18 10:24:30 -0500

    modified files

--- Contacts.java
+++ Contacts.java
@@ -7,46 +7,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.nio.file.Files.readAllLines;
-import static java.nio.file.Files.write;
-
 public class Contacts {
 
-//******* Was getting errors because I forgot to add "throws IOException to my psvm  ********
-                                          //V V V V V V V V V
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
-        //create variables to hold our chosen file names
         String directory = "data";
         String filename = "contacts.txt";
 
-        //create the paths we want to put them in
         Path dataDirectory = Paths.get(directory);
+
         Path dataFile = Paths.get(directory, filename);
 
-        //checks if directory and file actually exist, creates them
         if (Files.notExists(dataDirectory)) {
             Files.createDirectories(dataDirectory);
         }
+
         if (! Files.exists(dataFile)) {
             Files.createFile(dataFile);
         }
 
-        //create new list and add names
-// ********GONNA NEED TO FIGURE OUT HOW YOU GUYS WANNA ADD NUMBERS TO IT********
-        List<String> contactsList = Arrays.asList("Ralph", "Crystal", "Antonio");
+        List<String> contacts = Arrays.asList("Ralph", "Crystal", "Antonio");
 
-        //creates file path to store the path in order to use it with our write
         Path filepath = Paths.get("data", "contacts.txt");
-        write(filepath, contactsList);
 
-        // create new list to read from the dataFile
-        List<String> contacts = Files.readAllLines(filepath);
+        Files.write(filepath, contacts);
 
-        //check what we got so far
-        System.out.println(contacts);
+        List<String> contactsList = Files.readAllLines(filepath);
+
+        System.out.println("contacts = " + filename);
+
+        for (int i = 0; i < contactsList.size(); i += 1) {
+
+            System.out.println((i + 1) + ": " + contactsList.get(i));
+
+        }
+
+        Files.write(
+                filepath,
+                List.of("John"), // list with one item
+                StandardOpenOption.APPEND
+        );
 
 
+        System.out.println("contacts = " + contactsList);
+
+
+        List<String> lines = Files.readAllLines(filepath);
+
+        List<String> newList = new ArrayList<>();
+
+//        for (String line : lines) {
+//            if (line.equals("milk")) {
+//                newList.add("cream");
+//                continue;
+//            }
+//            newList.add(line);
+//        }
+
+        Files.write(filepath, newList);
     }
-
 }
